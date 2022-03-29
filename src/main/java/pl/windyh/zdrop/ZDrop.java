357b50a01c18eb81a58e23a62bcdb6bc5d61d2c5
unknown <m.zych06@gmail.com>
2022-03-29 17:32:58 +0200

    first commit

---
+++ src/main/java/pl/windyh/zdrop/ZDrop.java
@@ -0,0 +1,19 @@
+package pl.windyh.zdrop;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ZDrop extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("===: zDrop is loading...");
+
+
+        System.out.println("===: zDrop loaded!");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
